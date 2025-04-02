3e844bd551296a4b4ce2da30b56d6181a2ff961c
Elzari <elzari.skinner@gmail.com>
2025-04-02 12:52:18 +0200

    Add to favorites popup

--- src/app/components/tab-menu/tab-menu.component.ts
+++ src/app/components/tab-menu/tab-menu.component.ts
@@ -12,7 +12,8 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 })
 export class TabMenuComponent implements AfterViewInit {
 
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {
+  }
 
   selectedTab: string = 'dashboard';  // Default active tab
   autosave: boolean = false;
@@ -23,59 +24,58 @@ export class TabMenuComponent implements AfterViewInit {
   selectedOption: string = 'Collapsed';
   activePanel: string = 'homeFields';
 
-  sideBarOpen:boolean = false;
+  sideBarOpen: boolean = false;
   // fieldVisible:boolean = false;
-  fields :{icon: string , name:string, enabled:boolean}[] = [
-    {icon: 'bi-person' , name : 'Assignee', enabled:true},
-    {icon:'bi-caret-down-square', name : 'Phase', enabled:true},
-    {icon:'bi-caret-down-square', name : 'Created By', enabled:true},
-    {icon:'bi-caret-down-square' , name : 'UX - Status', enabled:true},
-    {icon:'bi-caret-down-square', name : 'Development Status', enabled:true},
-    {icon:'bi-caret-down-square', name : 'Overall Status', enabled:true},
-    {icon:'bi-flag', name : 'Priority', enabled:true},
-    {icon:'bi-calendar3', name : 'Due date', enabled:true},
-    {icon:'bi-caret-down-square', name : 'Type', enabled:true},
-    {icon:'bi-star', name : 'Effort', enabled:true},
-    {icon:'bi-hash', name : 'Sequence', enabled:true},
+  fields: { icon: string, name: string, enabled: boolean }[] = [
+    {icon: 'bi-person', name: 'Assignee', enabled: true},
+    {icon: 'bi-caret-down-square', name: 'Phase', enabled: true},
+    {icon: 'bi-caret-down-square', name: 'Created By', enabled: true},
+    {icon: 'bi-caret-down-square', name: 'UX - Status', enabled: true},
+    {icon: 'bi-caret-down-square', name: 'Development Status', enabled: true},
+    {icon: 'bi-caret-down-square', name: 'Overall Status', enabled: true},
+    {icon: 'bi-flag', name: 'Priority', enabled: true},
+    {icon: 'bi-calendar3', name: 'Due date', enabled: true},
+    {icon: 'bi-caret-down-square', name: 'Type', enabled: true},
+    {icon: 'bi-star', name: 'Effort', enabled: true},
+    {icon: 'bi-hash', name: 'Sequence', enabled: true},
 
   ]
-  popularFields:{icon:string, name:string, enabled:boolean}[] = [
-    {icon:'bi-chat', name : 'Comments', enabled:false},
-    {icon:'bi-calendar3', name : 'Date created', enabled:false},
-    {icon:'bi-hash', name : 'Pull request', enabled:false},
+  popularFields: { icon: string, name: string, enabled: boolean }[] = [
+    {icon: 'bi-chat', name: 'Comments', enabled: false},
+    {icon: 'bi-calendar3', name: 'Date created', enabled: false},
+    {icon: 'bi-hash', name: 'Pull request', enabled: false},
   ]
 
-  hidden:{icon:string, name:string, enabled:boolean}[] = [
-    {icon:'bi-chat', name : 'Assigned Comments', enabled:false},
-    {icon:'bi-person', name : 'Created by', enabled:false},
-    {icon:'bi-calendar3', name : 'Date closed', enabled:false},
-    {icon:'bi-calendar3', name : 'Date Updated', enabled:false},
-    {icon:'bi-hash', name : 'Dependencies', enabled:false},
-    {icon:'bi-person', name : 'Latest comment', enabled:false},
-    {icon:'bi-calendar3', name : 'Linked Docs', enabled:false},
-    {icon:'bi-calendar3', name : 'Linked tasks', enabled:false},
-    {icon:'bi-hash', name : 'Lists', enabled:false},
+  hidden: { icon: string, name: string, enabled: boolean }[] = [
+    {icon: 'bi-chat', name: 'Assigned Comments', enabled: false},
+    {icon: 'bi-person', name: 'Created by', enabled: false},
+    {icon: 'bi-calendar3', name: 'Date closed', enabled: false},
+    {icon: 'bi-calendar3', name: 'Date Updated', enabled: false},
+    {icon: 'bi-hash', name: 'Dependencies', enabled: false},
+    {icon: 'bi-person', name: 'Latest comment', enabled: false},
+    {icon: 'bi-calendar3', name: 'Linked Docs', enabled: false},
+    {icon: 'bi-calendar3', name: 'Linked tasks', enabled: false},
+    {icon: 'bi-hash', name: 'Lists', enabled: false},
   ]
 
-  fieldsInWorkspace:{icon:string, name:string, enabled:boolean}[] = [
-    {icon:'bi-chat', name : 'Probability', enabled:false},
-    {icon:'bi-person', name : 'Risk Level', enabled:false},
-    {icon:'bi-calendar3', name : 'Duration(Days)', enabled:false},
-    {icon:'bi-calendar3', name : 'Approver', enabled:false},
-    {icon:'bi-hash', name : 'Project Aspect', enabled:false},
-    {icon:'bi-person', name : 'Description', enabled:false},
-    {icon:'bi-calendar3', name : 'Mitigation cost', enabled:false},
-
-  ]
-
-  creatingNewFieldsItems:{icon:string, name:string, enabled:boolean}[] = [
-    {icon:'bi-chat', name : 'Destination', enabled:false},
-    {icon:'bi-person', name : 'Customer Feedback', enabled:false},
-    {icon:'bi-calendar3', name : 'Tourism Phase', enabled:false},
-    {icon:'bi-calendar3', name : 'Task summary', enabled:false},
+  fieldsInWorkspace: { icon: string, name: string, enabled: boolean }[] = [
+    {icon: 'bi-chat', name: 'Probability', enabled: false},
+    {icon: 'bi-person', name: 'Risk Level', enabled: false},
+    {icon: 'bi-calendar3', name: 'Duration(Days)', enabled: false},
+    {icon: 'bi-calendar3', name: 'Approver', enabled: false},
+    {icon: 'bi-hash', name: 'Project Aspect', enabled: false},
+    {icon: 'bi-person', name: 'Description', enabled: false},
+    {icon: 'bi-calendar3', name: 'Mitigation cost', enabled: false},
 
   ]
 
+  creatingNewFieldsItems: { icon: string, name: string, enabled: boolean }[] = [
+    {icon: 'bi-chat', name: 'Destination', enabled: false},
+    {icon: 'bi-person', name: 'Customer Feedback', enabled: false},
+    {icon: 'bi-calendar3', name: 'Tourism Phase', enabled: false},
+    {icon: 'bi-calendar3', name: 'Task summary', enabled: false},
+
+  ]
 
 
   fieldVisible: boolean = false;
@@ -85,16 +85,15 @@ export class TabMenuComponent implements AfterViewInit {
   }
 
 
-
-  layouts: {  name: string, enabled: boolean }[] = [
-    {  name: 'Show empty statuses', enabled: true },
-    {  name: 'Pin Description', enabled: true },
-    {name: 'Wrap Text', enabled: true },
-    { name: 'Task location', enabled: true },
-    {  name: 'Task properties', enabled: true },
-    { name: 'Task locations', enabled: true },
-    { name: 'Task properties', enabled: true },
-    { name: 'Subtask parent names', enabled: true },
+  layouts: { name: string, enabled: boolean }[] = [
+    {name: 'Show empty statuses', enabled: true},
+    {name: 'Pin Description', enabled: true},
+    {name: 'Wrap Text', enabled: true},
+    {name: 'Task location', enabled: true},
+    {name: 'Task properties', enabled: true},
+    {name: 'Task locations', enabled: true},
+    {name: 'Task properties', enabled: true},
+    {name: 'Subtask parent names', enabled: true},
 
   ];
 
@@ -153,8 +152,14 @@ export class TabMenuComponent implements AfterViewInit {
     this.selectedOption = option;
   }
 
-  toggleSideBar(){
+  toggleSideBar() {
     this.sideBarOpen = !this.sideBarOpen;
+  }
+
+  favoriteName: string = '';
+
+  saveFavorite() {
+    console.log('Favorite saved:', this.favoriteName);
   }
 
 
