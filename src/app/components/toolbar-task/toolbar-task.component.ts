a141443e24097a1a93d56dbed6546b053d323d05
Mzwa1080 <mshawnlouw@gmail.com>
2025-04-08 19:45:46 +0200

    installed bootstrap as dev dep.. for modal

--- src/app/components/toolbar-task/toolbar-task.component.ts
+++ src/app/components/toolbar-task/toolbar-task.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import { Modal } from "bootstrap";
 
 
 @Component({
@@ -12,9 +13,13 @@ import {FormsModule} from '@angular/forms';
   // styleUrl: './toolbar-task.component.css'
 })
 export class ToolbarTaskComponent {
-
+  @ViewChild('myModal', {static:true}) modalRef!: ElementRef;
   searchQuery: string = '';
 
+  openModal(){
+    let modal = new Modal(this.modalRef.nativeElement);
+    modal.show()
+  }
 
   onSearch(): void {
     console.log('Search initiated for:', this.searchQuery);
