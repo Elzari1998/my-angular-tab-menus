766748a9ad3ac9458a230fc33344653768324943
Elzari <elzari.skinner@gmail.com>
2025-04-08 11:38:42 +0200

    Move new button to own component

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,43 +1,20 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {ToolbarTaskComponent} from "../toolbar-task/toolbar-task.component";
-import {ToolbarReminderComponent} from "../toolbar-reminder/toolbar-reminder.component";
-import {ToolbarChatComponent} from "../toolbar-chat/toolbar-chat.component";
-import {ToolbarDocComponent} from "../toolbar-doc/toolbar-doc.component";
-import {ToolbarWhiteboardComponent} from "../toolbar-whiteboard/toolbar-whiteboard.components";
-import {ToolbarDashboardComponent} from "../toolbar-dashboard/toolbar-dashboard.component";
+import {ToolbarNewButtonComponent} from "../toolbar-newButton/toolbar-newButton.component";
 
 @Component({
   selector: 'app-toolbar',
 
   imports: [
     FormsModule,
-    ToolbarTaskComponent,
-    ToolbarDocComponent,
-    ToolbarReminderComponent,
-    ToolbarChatComponent,
-    ToolbarDocComponent,
-    ToolbarDocComponent,
-    ToolbarWhiteboardComponent,
-    ToolbarDocComponent,
-    ToolbarWhiteboardComponent,
-    ToolbarDashboardComponent
+    ToolbarNewButtonComponent,
+
   ],
   templateUrl: './toolbar.component.html',
   styleUrl: './toolbar.component.css'
 })
 
 export class ToolbarComponent {
-  @ViewChild('modalContent', {static: false}) modalContent!: ElementRef;
   searchQuery: any;
 
-  ngAfterViewInit() {
-    document.querySelectorAll('.nav-link').forEach(tab => {
-      tab.addEventListener('click', () => {
-        if (this.modalContent) {
-          this.modalContent.nativeElement.scrollTop = 0;
-        }
-      });
-    });
-  }
 }
