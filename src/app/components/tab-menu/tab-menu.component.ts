e6f4381d20c76104244ca4921b16223a46737994
Mzwa1080 <mshawnlouw@gmail.com>
2025-04-08 15:57:01 +0200

    added export view component

--- src/app/components/tab-menu/tab-menu.component.ts
+++ src/app/components/tab-menu/tab-menu.component.ts
@@ -9,11 +9,13 @@ import {GroupsComponent} from '../groups/groups.component';
 import {SubtasksComponent} from '../subtasks/subtasks.component';
 import {TemplatesComponent} from '../templates/templates.component';
 import {AddToFavoritesComponent} from '../add-to-favorites/add-to-favorites.component';
+import {ExportViewComponent} from '../export-view/export-view.component';
 
 @Component({
   selector: 'app-tab-menu',
   imports: [CommonModule, FormsModule, MatSlideToggleModule, LayoutOptionsComponent,
-    FieldsComponent, FilterComponent, GroupsComponent, SubtasksComponent, TemplatesComponent,AddToFavoritesComponent],
+    FieldsComponent, FilterComponent, GroupsComponent, SubtasksComponent, TemplatesComponent,
+    AddToFavoritesComponent, ExportViewComponent],
   templateUrl: './tab-menu.component.html',
   styleUrls: ['./tab-menu.component.css']
 })
@@ -33,29 +35,6 @@ export class TabMenuComponent implements AfterViewInit {
   // favoriteName = '';
 
 
-
-  exportOptionsList = [
-    { icon: '', name: 'Visible columns' },
-    { icon: '', name: 'Task Names Only' },
-    { icon: '', name: 'All columns' }
-  ];
-
-  fileFormat = [
-    { icon: '', name: 'CSV' },
-    { icon: '', name: 'Excel' }
-  ];
-
-  dateFormat = [
-    { icon: '', name: 'Normal' },
-    { icon: '', name: 'ISO' },
-    { icon: '', name: 'POSIX' }
-  ];
-
-  timeFormat = [
-    { icon: '', name: 'Normal' },
-    { icon: '', name: 'hh:mm' },
-    { icon: '', name: 'hh:mm:ss' }
-  ];
 
   users = [
     { id: '1', name: 'John Doe' },
@@ -97,9 +76,9 @@ export class TabMenuComponent implements AfterViewInit {
 
 
 
-  preventRefreshingThePage(event: Event): void {
-    event.preventDefault();
-  }
+  // preventRefreshingThePage(event: Event): void {
+  //   event.preventDefault();
+  // }
 
   // saveFavorite(): void {
   //   console.log('Favorite saved:', this.favoriteName);
