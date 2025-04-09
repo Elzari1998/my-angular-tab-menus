602d20b8fde13e9bf734fc394b4173aeb562b9c4
Elzari <elzari.skinner@gmail.com>
2025-04-09 13:26:35 +0200

    Added Filter Items

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import {registerLicense} from '@syncfusion/ej2-base';
 
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NNaF5cXmBCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWXteeXRQRGNcVUN2X0JWYEo=');
+
+
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
 
-registerLicense('Ngo9BigBOggjHTQxAR8/V1NDaF5cWWtCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdnWH9fdHRURmlfUkJ+XUE=');
+
