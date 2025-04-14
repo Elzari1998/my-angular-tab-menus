cc1b0f2ad65b47a7ebc2d0ff8d7e8f2ed1ef7768
Richterblom <88100418+Richterblom@users.noreply.github.com>
2025-04-14 10:41:05 +0200

    fix: fix doc

--- src/app/components/toolbar-doc/toolbar-doc.component.ts
+++ src/app/components/toolbar-doc/toolbar-doc.component.ts
@@ -37,7 +37,9 @@ import {
 })
 export class ToolbarDocComponent implements AfterViewInit {
   showDocumentEditor = false;
+  // Variable to track visibility of the input box
   isInputVisible: boolean = false;
+  // Variable to store the input field value
   inputValue: string = '';
 
   @ViewChild('document_editor', { static: false })
