017798d5b1ed33929a3159ac02a2ea3eed3763ac
Richterblom <88100418+Richterblom@users.noreply.github.com>
2025-04-14 09:24:19 +0200

    fix: commented out redundant code & customized Doc tab

--- src/app/components/toolbar-doc/toolbar-doc.component.ts
+++ src/app/components/toolbar-doc/toolbar-doc.component.ts
@@ -1,16 +1,20 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import {CommonModule} from '@angular/common';
 
 @Component({
   selector: 'app-toolbar-doc',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,CommonModule],
   templateUrl: './toolbar-doc.component.html',
   styleUrls: ['./toolbar-doc.component.css'],
 })
 export class ToolbarDocComponent implements AfterViewInit {
   @ViewChild('modalBody', { static: false }) modalBody!: ElementRef;
-
+  // Variable to track visibility of the input box
+  isInputVisible: boolean = false;
+  // Variable to store the input field value
+  inputValue: string = '';
   ngAfterViewInit() {
     if (typeof document !== 'undefined') {
       document.querySelectorAll('.nav-link').forEach((tab) => {
@@ -21,5 +25,19 @@ export class ToolbarDocComponent implements AfterViewInit {
         });
       });
     }
+  }
+
+
+  // Method to toggle the input box visibility
+  toggleInput(): void {
+    this.isInputVisible = !this.isInputVisible;
+  }
+
+  createDoc(): void {
+    // Hide the input field and show the label and icon again
+    this.isInputVisible = false;
+    // Clear the input field
+    this.inputValue = '';
+
   }
 }
